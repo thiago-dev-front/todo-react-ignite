df5978c501342dfa9f7b8a2087ad77ba824a972a
Thiago <aizen.maia@gmail.com>
2022-12-14 15:47:06 -0300

    header

---
+++ src/Header/index.tsx
@@ -0,0 +1,20 @@
+import todoLogo from "../assets/todo_logo.svg";
+import { AiOutlinePlusCircle} from "react-icons/ai"
+import styles from "./header.module.css";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={todoLogo} />
+
+      <form className={styles.newTaskForm}>
+        <input placeholder="Adicione uma nova tarefa" />
+        <button>
+          Criar
+        <AiOutlinePlusCircle size={20}/>
+        </button>
+      </form>
+      
+    </header>
+  );
+}
