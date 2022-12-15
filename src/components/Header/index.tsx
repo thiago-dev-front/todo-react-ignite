6bdf39be8abc070287c462129bfcc69b9940dfed
Thiago <aizen.maia@gmail.com>
2022-12-15 17:32:23 -0300

    ajustes finais todo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,39 @@
 import todoLogo from "../../assets/todo_logo.svg";
-import { AiOutlinePlusCircle} from "react-icons/ai"
+import { AiOutlinePlusCircle } from "react-icons/ai";
 import styles from "./header.module.css";
+import { ChangeEvent, FormEvent, useState } from "react";
 
-export function Header() {
+interface Props {
+  onAddTask: (taskTitle: string) => void;
+}
+export function Header({ onAddTask }: Props) {
+  const [title, setTitle] = useState("");
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    onAddTask(title);
+    setTitle("");
+  }
+
+  function onChangeTitle(event: ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value);
+  }
+
   return (
     <header className={styles.header}>
       <img src={todoLogo} />
 
-      <form className={styles.newTaskForm}>
-        <input placeholder="Adicione uma nova tarefa" />
+      <form className={styles.newTaskForm} onSubmit={handleSubmit}>
+        <input
+          placeholder="Adicione uma nova tarefa"
+          onChange={onChangeTitle}
+          value={title}
+        />
         <button>
           Criar
-        <AiOutlinePlusCircle size={20}/>
+          <AiOutlinePlusCircle size={20} />
         </button>
       </form>
-      
     </header>
   );
 }
--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,16 +1,28 @@
 import styles from "./task.module.css";
-import {TbTrash} from "react-icons/tb"
+import { TbTrash } from "react-icons/tb";
+import { ITask } from "../../App";
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
-export function Task() {
-    return (
-        <div className={styles.task}>
-            <button className={styles.checkContainer} >
-                <div />
-            </button>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minima maxime vel aut ad dolorum.</p>
-            <button className={styles.deleteButton} >
-                <TbTrash  size={20}/>
-            </button>
-        </div>
-    )
+interface Props {
+  task: ITask;
+  onDelete: (taskId: string) => void;
+  onComplete: (taskId: string) => void;
+}
+export function Task({ task, onDelete, onComplete }: Props) {
+  return (
+    <div className={styles.task}>
+      <button
+        className={styles.checkContainer}
+        onClick={() => onComplete(task.id)}
+      >
+        {task.isCompleted ? <BsFillCheckCircleFill /> : <div />}
+      </button>
+      <p className={task.isCompleted ? styles.textCompleted : ""}>
+        {task.title}
+      </p>
+      <button className={styles.deleteButton} onClick={() => onDelete(task.id)}>
+        <TbTrash size={20} />
+      </button>
+    </div>
+  );
 }
